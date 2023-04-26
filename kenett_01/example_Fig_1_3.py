f4e1f9b2a5af070579805b6bed5d4f1658067162
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-04-26 17:43:53 +0700

    Fixes

--- kenett_01/example_Fig_1_3.py
+++ kenett_01/example_Fig_1_3.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import math
 from scipy.stats import norm
 
@@ -6,12 +7,12 @@ from scipy.stats import norm
 x = [math.sin(x*2*math.pi/50) for x in range(1,51)]
 
 # Add random, normal distributed, components
-x = [xi + norm.rvs(loc=0, scales=0.05) for xi in x]
+x = [xi + norm.rvs(loc=0, scale=0.05) for xi in x]
 
 # Make a plot via Pandas
 # Create a Series first, then plot
 ax = pd.Series(x).plot(style=".", color="black")
 ax.set_ylabel("Values")
-ax.hline(y=0, linestyle="--", color="darkgray")
+ax.axhline(y=0, linestyle="--", color="darkgray")
 plt.savefig("IMG_Fig_1_3.png", dpi=150)
 plt.show()
